8ab6026ff125af675219a959678c82cedc2585ea
Ethan Mullen <ethan_mullen@brown.edu>
2020-10-18 22:58:31 -0400

    full website !!

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
 toggleButton.addEventListener("click", () => {
+  console.log("activated");
   navbarLinks.classList.toggle("active");
 });
